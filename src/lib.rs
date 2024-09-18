4b0c25df1988479802a89c0f71234eadf1f3fda0
John Christensen <johnedchristensen@gmail.com>
2024-09-17 22:24:44 -0500

    Add mutable State

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,20 @@ pub fn innit() -> Vibe {
     return Vibe { tree: Tree {} };
 }
 
+#[derive(Debug)]
 pub struct Vibe {
     tree: Tree,
 }
 impl Vibe {
-    pub fn render(self) -> Self {
-        Vibe {
-            tree: self.tree.render(),
-        }
+    pub fn render(&mut self) {
+        self.tree.render();
     }
 }
 
 //// Tree
+#[derive(Debug)]
 pub struct Tree {}
 
 impl Tree {
-    pub fn render(self) -> Self {
-        self
-    }
+    pub fn render(&mut self) {}
 }
--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,58 @@ use vibe::{innit, Vibe};
 
 fn main() {
     let vibe = innit();
-    let mut state = State { vibe };
+    let mut state = State {
+        vibe,
+        cube: Cube {
+            position: Position::new(0., 0., 0.),
+            length: 1.,
+        },
+    };
     loop {
-        state = state.update();
+        state.update();
+        println!("{state:?}");
+        state.vibe.render();
     }
 }
 
+#[derive(Debug)]
 struct State {
     vibe: Vibe,
+    cube: Cube,
+}
+
+#[derive(Debug)]
+struct Position {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+impl Position {
+    fn new(x: f32, y: f32, z: f32) -> Self {
+        Self { x, y, z }
+    }
+
+    pub fn add(&mut self, p2: Position) {
+        self.x += p2.x;
+        self.y += p2.y;
+        self.z += p2.z;
+    }
+}
+
+#[derive(Debug)]
+struct Cube {
+    position: Position,
+    length: f32,
 }
 
 impl State {
-    fn update(self) -> Self {
-        State {
-            vibe: self.vibe.render(),
-        }
+    fn update(&mut self) {
+        self.cube.position.add(Position {
+            x: 0.,
+            y: 1.,
+            z: 0.,
+        });
+        self.cube.length += 0.001;
     }
 }
